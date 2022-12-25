fad610aad6914c746b05585130814a480cc53bff
Keyzzzt <keyzzzt@gmail.com>
2022-12-25 12:07:34 +0200

    ...

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-let a;
+/**
+ * ! Удаление дубликатов из массива
+ */
+{
+  let chars = ["A", "B", "A", "C", "B"]
+  let uniqueChars = [...new Set(chars)]
+}
+
+/**
+ * ! Длина Set
+ */
+{
+  let chars = ["A", "B", "A", "C", "B"]
+  let uniqueCharsLength = [...new Set(chars)].size
+}
